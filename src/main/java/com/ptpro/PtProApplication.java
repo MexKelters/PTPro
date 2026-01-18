2517af32e70b5ad3a1e40e426c46ad6e9da3095e
MexNL <mex_kelters@hotmail.com>
2026-01-18 21:50:00 +0100

    Project made

---
+++ src/main/java/com/ptpro/PtProApplication.java
@@ -0,0 +1,13 @@
+package com.ptpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtProApplication.class, args);
+    }
+
+}
