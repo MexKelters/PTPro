242cad051e6756cf4ce015139f7fc794165f1a27
Mex <mex_kelters@hotmail.com>
2026-03-10 16:24:28 +0100

    fix on updateUserRequest

--- src/main/java/com/ptpro/dto/request/UpdateUserRequest.java
+++ src/main/java/com/ptpro/dto/request/UpdateUserRequest.java
@@ -5,15 +5,6 @@ public class UpdateUserRequest {
     private String lastName;
     private String email;
     private Long roleId;
-    private String password;
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public String getFirstName() {
         return firstName;
